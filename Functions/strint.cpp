08c879447d1e311131811b9677c0a51066c5b525
criscfer <43559766+criscfer@users.noreply.github.com>
2018-10-14 21:48:44 -0700

    Add files via upload

---
+++ Functions/strint.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<vector>
+#include<string>
+
+int strint(std::string str) // Function to convert strings to integers 
+{
+	bool converted{true};
+	int num{};
+	std::vector<int> numbers{};
+	for(int j{0}; j < 100; j++)
+	{
+		numbers.push_back(j);	
+	}
+	num = std::stoi(str);
+	size_t i{0};
+	while(i < numbers.size())
+	{
+		if(numbers.at(i) == num)
+		{
+			converted = true;
+		}
+		else
+		{
+			converted = false;
+			break;
+		}
+		i++;
+	}
+	if(converted == true)
+	{
+		return num;
+	}
+	else
+	{
+		return -1;
+	}
+}
+
+int main()
+{
+	int number{};
+	std::string str{};
+	std::cin >> str;
+	number = strint(str);
+	if(number == -1)
+	{
+		std::cout<< "Error! Invalid number!" << std::endl;
+	}
+	else
+	{
+		std::cout<< number;
+	}
+	return 0;
+}
